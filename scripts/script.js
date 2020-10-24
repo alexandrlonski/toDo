ce9ff0152b936f6c49eb7d9a1df1a90e92062a1a
Alexandr Lonski <aleksandr.lonski.86@gmail.com>
2020-10-24 11:15:25 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+'use stricts'
+
+const todoControl = document.querySelector('.todo-control'),
+      headerInput = document.querySelector('.header-input'),
+      todoList = document.querySelector('.todo-list'),
+      todoCompleted = document.querySelector('.todo-completed');
+
+const todoData =  localStorage.getItem('todo') ? JSON.parse(locoalStorage.getItem('todo')) : [] ; 
+
+
+const render = function() {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+  todoData.forEach(function(item){
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+                   '<div class="todo-buttons">' +
+                      '<button class="todo-remove"></button>' +
+                      '<button class="todo-complete"></button>' +
+                   '</div>';
+      if(item.value.trim() !== '') {
+          if(item.completed){
+       todoCompleted.append(li);
+       localStorage.setItem('todo', JSON.stringify(todoData));
+     } else {
+       todoList.append(li);
+        localStorage.setItem('todo', JSON.stringify(todoData));
+     }
+      }
+
+     const btnTodoComplete = li.querySelector('.todo-complete');
+     btnTodoComplete.addEventListener('click', function() {
+       item.completed = !item.completed ;
+       render();
+     });
+     const btnTodoDelete = li.querySelector('.todo-remove');
+     btnTodoDelete.addEventListener('click', function() {
+      li.remove(li);
+      let index = todoData.indexOf(item);
+      if (index > -1) {
+        todoData.splice(index, 1);
+      }
+      localStorage.clear();
+      render();
+     });
+
+                   
+                   
+  });
+
+};
+todoControl.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const newTodo = {
+    value: headerInput.value,
+    completed: false
+
+  };
+
+  todoData.push(newTodo);
+  
+   headerInput.value = '';
+  render();
+  
+  
+ 
+} );
+
+ 
+ render();
